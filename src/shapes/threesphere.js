563ccfb4953960b754b5cf97dc74fe34652fa190
Francois Poizat <francois.poizat@gmail.com>
2021-02-11 13:06:37 +0100

    threesphere with correct faces and cells

--- src/shapes/threesphere.js
+++ src/shapes/threesphere.js
@@ -1,4 +1,4 @@
-function createSphere(radius, step=Math.PI/8) {
+function createSphere(radius, piResolution=8) {
   const vertices = [];
   const faces = [];
   const cells = [];
@@ -10,9 +10,10 @@ function createSphere(radius, step=Math.PI/8) {
     gamma: {},
   }
 
-  for (let theta = 0; theta <=  Math.PI; theta += step) {
-    for (let phi = 0; phi <=  Math.PI; phi += step) {
-      for (let gamma = 0; gamma <= 2 * Math.PI; gamma += step) {
+  for (let theta = 0; theta <=  piResolution; theta++) {
+    for (let phi = 0; phi <=  piResolution; phi++) {
+      for (let gamma = 0; gamma < 2 * piResolution; gamma++) {
+      //for (let gamma = 0; gamma < 2 * piResolution; gamma+=piResolution) {
         if (!classOfVertex.theta[theta]) {
           classOfVertex.theta[theta] = [];
         }
@@ -23,15 +24,20 @@ function createSphere(radius, step=Math.PI/8) {
           classOfVertex.gamma[gamma] = [];
         }
 
+
         classOfVertex.theta[theta].push(vertices.length)
         classOfVertex.phi[phi].push(vertices.length)
         classOfVertex.gamma[gamma].push(vertices.length)
 
+        const thetaInPi = theta * Math.PI / piResolution;
+        const phiInPi = phi * Math.PI / piResolution;
+        const gammaInPi = gamma * Math.PI / piResolution;
+
         vertices.push([
-          radius * Math.cos(theta),
-          radius * Math.sin(theta) * Math.cos(phi),
-          radius * Math.sin(theta) * Math.sin(phi) * Math.cos(gamma),
-          radius * Math.sin(theta) * Math.sin(phi) * Math.sin(gamma),
+          radius * Math.cos(thetaInPi),
+          radius * Math.sin(thetaInPi) * Math.cos(phiInPi),
+          radius * Math.sin(thetaInPi) * Math.sin(phiInPi) * Math.cos(gammaInPi),
+          radius * Math.sin(thetaInPi) * Math.sin(phiInPi) * Math.sin(gammaInPi),
         ]);
 
       }
@@ -40,50 +46,81 @@ function createSphere(radius, step=Math.PI/8) {
 
   for (let key in classOfVertex.theta) {
     const vertexClass = classOfVertex.theta[key];
-    for (let i = 0; i < vertexClass.length; i++) {
-      faces.push([
-        vertexClass[i],
-        vertexClass[(i + 1) % vertexClass.length],
-        vertexClass[(i + 2 + (2 * Math.PI / step)) % vertexClass.length],
-        vertexClass[(i + 1 + (2 * Math.PI / step)) % vertexClass.length],
-      ])
-      cells.push(facesIndex);
-      facesIndex++;
+    const cell = [];
+    for (let rounds = 0; rounds < vertexClass.length / (piResolution * 2) - 1; rounds++) {
+      for (let i = 0; i < piResolution * 2; i++) {
+        let iBase = rounds * piResolution * 2;
+        let iBaseNext = (rounds + 1) * piResolution * 2;
+        faces.push([
+          vertexClass[iBase + i],
+          vertexClass[iBase + ((i + 1) % (piResolution * 2))],
+          vertexClass[iBaseNext + ((i + 1) % (piResolution * 2))],
+          vertexClass[iBaseNext + (i % (piResolution * 2))],
+        ])
+        cell.push(facesIndex);
+        facesIndex++;
+      }
     }
+    cells.push(cell);
   }
 
   for (let key in classOfVertex.phi) {
     const vertexClass = classOfVertex.phi[key];
-    for (let i = 0; i < vertexClass.length; i++) {
-      faces.push([
-        vertexClass[i],
-        vertexClass[(i + 1) % vertexClass.length],
-        vertexClass[(i + 2 + (2 * Math.PI / step)) % vertexClass.length],
-        vertexClass[(i + 1 + (2 * Math.PI / step)) % vertexClass.length],
-      ])
-      cells.push(facesIndex);
-      facesIndex++;
+    const cell = [];
+    for (let rounds = 0; rounds < vertexClass.length / (piResolution * 2) - 1; rounds++) {
+      for (let i = 0; i < piResolution * 2; i++) {
+        let iBase = rounds * piResolution * 2;
+        let iBaseNext = (rounds + 1) * piResolution * 2;
+        faces.push([
+          vertexClass[iBase + i],
+          vertexClass[iBase + ((i + 1) % (piResolution * 2))],
+          vertexClass[iBaseNext + ((i + 1) % (piResolution * 2))],
+          vertexClass[iBaseNext + (i % (piResolution * 2))],
+        ])
+        cell.push(facesIndex);
+        facesIndex++;
+      }
     }
+    cells.push(cell);
   }
 
   for (let key in classOfVertex.gamma) {
-    const vertexClass = classOfVertex.gamma[key];
-    for (let i = 0; i < vertexClass.length; i++) {
-      faces.push([
-        vertexClass[i],
-        vertexClass[(i + 1) % vertexClass.length],
-        vertexClass[(i + 2 + (Math.PI / step)) % vertexClass.length],
-        vertexClass[(i + 1 +  (Math.PI / step)) % vertexClass.length],
-      ])
-      cells.push(facesIndex);
-      facesIndex++;
+
+    if (parseInt(key) < piResolution) {
+      //we need to reconstruct the rounds from the classvertex
+      const vertexClassOne = classOfVertex.gamma[key];
+      const vertexClassTwo = classOfVertex.gamma[parseInt(key) + piResolution];
+
+      const vertexClass = []
+
+      for (let rounds = 0; rounds < vertexClassOne.length / (piResolution + 1); rounds++) {
+        vertexClass.push(...vertexClassOne.slice(rounds * (piResolution + 1), (rounds + 1) * (piResolution + 1)));
+        vertexClass.push(...vertexClassTwo.slice(rounds * (piResolution + 1), (rounds + 1) * (piResolution + 1)).reverse());
+      }
+
+      const cell = [];
+      for (let rounds = 0; rounds < (vertexClass.length / (2 * (piResolution + 1))) - 1; rounds++) {
+        for (let i = 0; i < 2 * (piResolution + 1); i++) {
+          let iBase = rounds * (2 * (piResolution + 1));
+          let iBaseNext = (rounds + 1) * (2 * (piResolution + 1));
+          faces.push([
+            vertexClass[iBase + i],
+            vertexClass[iBase + ((i + 1) % (2 * (piResolution + 1)))],
+            vertexClass[iBaseNext + ((i + 1) % (2 * (piResolution + 1)))],
+            vertexClass[iBaseNext + (i % (2 * (piResolution + 1)))],
+          ])
+          cell.push(facesIndex);
+          facesIndex++;
+        }
+      }
+      cells.push(cell);
     }
   }
 
   return {
     vertices,
     faces,
-    cells: [cells],
+    cells,
   }
 }
 
